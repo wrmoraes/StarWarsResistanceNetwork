11b6a78770fadbd100bbf02863df743dab945e1e
Moraes, Willian <willian.moraes@ehi.com>
2021-12-27 15:13:42 -0300

    Project Start - Initial Commit

---
+++ src/test/java/resistance/star/wars/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package resistance.star.wars.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
